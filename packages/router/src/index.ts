c5806334a796bec7b6542c9a7ba735e3f6452423
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-26 00:30:24 -0700

    context type

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,4 +1,7 @@
 import { Router, FileRouter } from "./router.ts";
+
 import { serveStatic } from "./serve-static.ts";
 
-export { Router, FileRouter, serveStatic };
+import type { Context } from "./router.ts";
+
+export { Router, FileRouter, serveStatic, type Context };
