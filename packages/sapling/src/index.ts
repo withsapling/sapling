904171eaad30ef8cde0c372d732fc09585f1b980
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-25 23:01:53 -0700

    trying landing layout module

--- packages/sapling/src/index.ts
+++ packages/sapling/src/index.ts
@@ -1,10 +1,14 @@
 import { Layout } from "./sapling.ts";
+import LandingLayout from "./LandingLayout.ts";
 
 // export html and raw helpers from hono
 export { html, raw } from "@hono/hono/html";
 
 // export types
 export type { LayoutProps, HtmlContent } from "./types/index.ts";
+
+// export layout module
+export { LandingLayout };
 
 // export functions
 export { Layout };
