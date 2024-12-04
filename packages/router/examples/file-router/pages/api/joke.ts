b068c18c7f4cdb019b13669ef51c8e6a65e71641
Jaydan Urwin <jaydan@treefarm.studio>
2024-12-04 11:55:58 -0700

    remove separate router package

--- packages/router/examples/file-router/pages/api/joke.ts
+++
@@ -1,5 +0,0 @@
-
-export default async function Joke(req: Request) {
-  const joke = await fetch("https://official-joke-api.appspot.com/random_joke");
-  return joke;
-}
--- packages/router/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router, FileRouter } from "./router.ts";
-
-import { serveStatic } from "./serve-static.ts";
-
-import type { Context } from "./router.ts";
-
-export { Router, FileRouter, serveStatic, type Context };
