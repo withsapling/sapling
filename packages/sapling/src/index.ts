5e95b0c06c54eb9b4c72ac91b32d802c95a93686
Jaydan Urwin <jaydan@treefarm.studio>
2024-12-07 23:14:12 -0700

    removed island from main package

--- packages/sapling/src/index.ts
+++ packages/sapling/src/index.ts
@@ -1,7 +1,6 @@
 import { Sapling } from "./sapling.ts";
 import { Layout } from "./sapling-layout.ts";
 import { serveStatic } from "./serve-static/index.ts";
-import { SaplingIsland } from "./sapling-island.ts";
 
 // export html and raw helpers from hono
 export { html, raw } from "@hono/hono/html";
@@ -14,9 +13,6 @@ export { Layout };
 
 // export serve-static function
 export { serveStatic };
-
-// export SaplingIsland
-export { SaplingIsland };
 
 // export types
 export type { LayoutProps, HtmlContent, Context } from "./types/index.ts";
