6e1f2491d21c880771a005e0a6616bb5c013938f
Jaydan Urwin <jaydan@treefarm.studio>
2025-02-04 12:37:08 -0700

    enable islands prop

--- packages/sapling/src/types/index.ts
+++ packages/sapling/src/types/index.ts
@@ -22,6 +22,10 @@ export interface LayoutProps {
    */
   disableTailwindReset?: boolean;
   /**
+   * Whether to enable Sapling Islands functionality
+   */
+  enableIslands?: boolean;
+  /**
    * The head content
    */
   head?: HtmlContent;
