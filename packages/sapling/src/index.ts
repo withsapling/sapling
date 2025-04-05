4a36b488874fb213816d474484280fe725b60298
Jaydan Urwin <jaydan@treefarm.studio>
2025-04-05 13:30:45 -0600

    deprecated

--- packages/sapling/src/index.ts
+++ packages/sapling/src/index.ts
@@ -1,9 +1,12 @@
 import { Layout } from "./sapling-layout.ts";
 import { ExperimentalJSXLayout } from "./experimental-jsx-layout.ts";
+import { Sapling } from "./sapling.ts";
 // export functions
 export { Layout };
 // export experimental jsx layout
 export { ExperimentalJSXLayout };
+// export Sapling class
+export { Sapling };
 
 // export prerender types
 // export type { PrerenderRoute, PrerenderOptions } from "./prerender/index.ts";
---
+++ packages/sapling/src/sapling.ts
@@ -0,0 +1,4 @@
+/**
+ * @deprecated Sapling is no longer a standalone router. We recommend using [Hono](https://hono.dev/) instead.
+ */
+export class Sapling {}
