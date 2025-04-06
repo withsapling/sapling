71925f6abb192cde8e43e37a53b820b5baee7191
Jaydan Urwin <jaydan@treefarm.studio>
2025-04-05 22:58:43 -0600

    extra details on doc

--- packages/sapling/src/sapling.ts
+++ packages/sapling/src/sapling.ts
@@ -1,4 +1,17 @@
 /**
- * @deprecated Sapling is no longer a standalone router. We recommend using [Hono](https://hono.dev/) instead.
+ * @deprecated Sapling is no longer a standalone router.
+ * 
+ * We recommend using [Hono](https://hono.dev/) instead.
+ * 
+ * @example
+ * ```ts
+ * import { Hono } from "@hono/hono";
+ * 
+ * const app = new Hono();
+ * 
+ * app.get("/", (c) => c.text("Hello World"));
+ * 
+ * export default app;
+ * ```
  */
 export class Sapling {}
