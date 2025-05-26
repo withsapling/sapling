e671a4c7ead0463f3a29453e21477ee3d1427bc5
Jaydan Urwin <jaydan@treefarm.studio>
2025-05-26 11:46:02 -0600

    cleanup

--- packages/sapling/src/index.ts
+++ packages/sapling/src/index.ts
@@ -1,13 +1,13 @@
 import { Layout } from "./sapling-layout.ts";
 import { Sapling } from "./sapling.ts";
 import { HtmlStreamLayout } from "./html-stream-layout.ts";
-import { SuspenseLayout } from "./suspense-layout.tsx";
+
 // export Default Layout
 export { Layout };
+
 // export Streaming Layout
 export { HtmlStreamLayout };
-// export Suspense Layout
-export { SuspenseLayout };
+
 // export Sapling class
 export { Sapling };
 
