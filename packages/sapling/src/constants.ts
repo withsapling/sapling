239b4ff0d6eb0f63f810e5799c7381fb1fb3f38d
Jaydan Urwin <jaydan@treefarm.studio>
2025-08-02 15:54:17 -0600

    types

--- packages/sapling/src/constants.ts
+++ packages/sapling/src/constants.ts
@@ -1,1 +1,1 @@
-export const SAPLING_VERSION = "0.8.0-beta.1";
+export const SAPLING_VERSION = "0.8.0-beta.2";
--- packages/sapling/src/index.ts
+++ packages/sapling/src/index.ts
@@ -1,2 +1,2 @@
 export * from "./sapling-layout.ts";
-
+export * from "./types/index.ts";
